9b3b5c17c64349906ecc219c3f2dcc7baf8f8a35
Pedro Vasconcelos <pbv@dcc.fc.up.pt>
2019-05-25 12:14:54 +0100

    added comments and cleaned up code

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,18 @@ import Test.Minicheck
 
 
 prop_even :: Int -> Int -> Bool
-prop_even x y = (x>100 && y>100) `implies` even (x+y)
+prop_even x y = (x>10 && y>10) `implies` even (x+y)
 
 implies x y = not x || y
 
+prop_other
+  = forAllShrink (choose (0,10)) shrink $ \x ->
+   forAllShrink (choose (0,10)) shrink $ \y ->
+      even (x + y :: Int)
+
+-- try: "checkTree prop_other"
+
 
 main :: IO ()
-main = check prop_even 
+main = do
+  check prop_even
